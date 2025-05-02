f208d97a3baaac59e5e7545c2120f0c0b2b95de1
Subash0610 <subash.s.maverick@gmail.com>
2025-05-02 14:34:32 +0530

    Create const.sv

---
+++ const.sv
@@ -0,0 +1,11 @@
+Write a constraint in 10 bit array , only 4 bits are 1 , no two ones should together
+Rand bit a[];
+
+Constraint c1 { constraint a.size == 10;}
+Constraint c2 {  foreach(a[I])
+		If(I>0) {
+		!(a[I] && a[I-1]);
+}
+}
+
+Constraint c3 {foreach(a[I]) a.sum () == 4;}
