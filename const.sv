8d8a3b9ebc302ef7ededb5787e08ba6791378ae3
Subash0610 <subash.s.maverick@gmail.com>
2025-05-02 17:14:23 +0530

    added create instance

--- const.sv
+++ const.sv
@@ -1,4 +1,5 @@
 Write a constraint in 10 bit array , only 4 bits are 1 , no two ones should together
+class const;
 Rand bit a[];
 
 Constraint c1 { constraint a.size == 10;}
@@ -9,3 +10,11 @@ Constraint c2 {  foreach(a[I])
 }
 
 Constraint c3 {foreach(a[I]) a.sum () == 4;}
+endclass
+
+module test;
+initial begin
+	const c = new();
+	assert(c.randomize());
+	end
+	endmodule
